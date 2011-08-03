fa6fa1d1ef2cc746e0436c8cf194e41f32ebda98
fenrhil <fenrhil@gmail.com>
2011-08-04 00:22:33 +0200

    License stuff

--- middleClickCloser@fenrhil.gmail.com/extension.js
+++ middleClickCloser@fenrhil.gmail.com/extension.js
@@ -1,4 +1,11 @@
-// Makes middle-click on a window in overlay mode close it.
+/* Makes middle-click on a window in overlay mode close it.
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ */
 const Workspace = imports.ui.workspace;
 
 /**
